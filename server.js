9e8088e27a5d1d6c4b0ef04250bb6ee7bddc52db
Beluga Bay <belugabay@belugas-mbp.lan>
2018-05-23 00:42:35 -0700

    comments

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express')
+var request = require('request')
+var bodyParser = require('body-parser')
 var app = express()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('bug_report', function(request, response) {
+app.get('/', function(request, response) {
   response.send('Hello World!')
 })
+
+
+
+
 
 
 app.listen(app.get('port'), function() {

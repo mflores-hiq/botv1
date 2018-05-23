3324f4add0606c4dc5e53d7b60e4aa98c021620a
Beluga Bay <belugabay@belugas-mbp.lan>
2018-05-23 00:59:14 -0700

    asda

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const bodyParser = require('body-parser');
 const app = express(); 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
-const server = app.listen(3000, () => { console.log('Express server listening on port %d in %s mode', server.address().port,   app.settings.env);});
+const server = app.listen(3000, () => { console.log('Express server   listening on port %d in %s mode', server.address().port,   app.settings.env);});
 
 
-app.post('/', (req, res) => { 
-let data = { 
-  response_type: 'in_channel', // public to the channel 
-  text: '302: Found', 
-  attachments:[ { 
-    image_url: 'https://http.cat/302.jpg' 
-  } ]}; 
-res.json(data);
-});
+app.get('/times', (req, res) => {
+  let result = ''
+  const times = process.env.TIMES || 5
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  res.send(result)
+})

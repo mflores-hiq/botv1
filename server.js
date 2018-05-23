109d871ab37df5aa9a8208ae1a514b8ddab12bcc
Beluga Bay <belugabay@belugas-mbp.lan>
2018-05-23 01:00:34 -0700

    asdas

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const server = app.listen(3000, () => { console.log('Express server   listening on port %d in %s mode', server.address().port,   app.settings.env);});
 
 
-app.get('/times', (req, res) => {
+app.get('/', (req, res) => {
   let result = ''
   const times = process.env.TIMES || 5
   for (i = 0; i < times; i++) {
